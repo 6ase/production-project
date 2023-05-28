b4df6ee4bcc175a25b9537b18d0a99a13dddd4c3
6ase <Serghome06@mail.ru>
2023-05-28 22:32:07 +0300

    Change Jest folder

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,29 @@
+
+import path from 'path';
+
+export default {
+	coveragePathIgnorePatterns: [
+		'<rootDir>\\\\node_modules\\\\'
+	],
+	coverageProvider: 'babel',
+	rootDir: '../../',
+	moduleDirectories: [ 'node_modules', 'src' ],
+	moduleFileExtensions: [
+		'js',
+		'mjs',
+		'cjs',
+		'jsx',
+		'ts',
+		'tsx',
+		'json',
+		'node'
+	],
+	testEnvironment: 'jsdom',
+	testMatch: [
+		'<rootDir>/**/*.?(spec|test).[tj]s?(x)',
+	],
+	moduleNameMapper: {
+		'\\.(css|scss)$': 'identity-obj-proxy',
+		'\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx')
+	},
+};
