39d5b03ece457346790a439761357ea1787513e1
6ase <Serghome06@mail.ru>
2023-06-28 12:41:03 +0300

    логика signin + backend

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,21 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import * as dotenv from 'dotenv';
+import { userRouter } from './routes/userRouter';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use('/', )
+app.use(cors({
+	credentials: true,
+	origin: true,
+}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.use('/user', userRouter)
 app.listen(port, () => {
 	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
